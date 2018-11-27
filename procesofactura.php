368d7182284a726720917740552078a8221c2753
Victor Tarkanyi <vtarkanyi@gmail.com>
2018-11-26 20:21:45 -0400

    Add files via upload

---
+++ procesofactura.php
@@ -0,0 +1,11 @@
+<?php
+
+	$nombre = $_GET['nombre'];
+	$FormatoCodigoRIF = $_GET['FormatoCodigoRIF'];
+	$RIFoCedula = $_GET['RIFoCedula'];
+	$DomicilioFiscal = $_GET['DomicilioFiscal'];
+	$Concepto = $_GET['Concepto / Descripcion del Servicio'];
+	$Precio = $_GET['nombre'];
+
+	echo "Hola mundo" . $nombre . " " . $FormatoCodigoRIF . " " . $RIFoCedula . " " . $DomicilioFiscal . " " . $Concepto . " " ;
+?>
